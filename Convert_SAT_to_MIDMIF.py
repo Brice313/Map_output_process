c7249a7fd685af154cf467f6dbc464d3a310a7c8
Brice313 <brice.cny@gmail.com>
2017-06-01 10:18:10 +0100

    Update Convert_SAT_to_MIDMIF.py
    modification as on june 1st

--- Convert_SAT_to_MIDMIF.py
+++ Convert_SAT_to_MIDMIF.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[588]:
+# In[136]:
 
 import pandas as pd
 
 
 # ## Read Network data
 
-# In[589]:
+# In[137]:
 
 fp_data=r"Input_files/Flowdata.KP"
 col_names=['A_node','B_node','data_1','data_2']
@@ -17,231 +17,192 @@ data=pd.read_fwf(fp_data,widths=col_widths, names=col_names,delim_whitespace=Tru
 data[["A_node","B_node"]]=data[["A_node","B_node"]].astype(str)
 
 
-# In[590]:
+# In[138]:
 
 data.info()
 
 
-# In[591]:
+# In[139]:
 
 data.head()
 
 
 # ## Read Coordinate data
 
-# In[592]:
+# In[140]:
 
 fp_coo=r"Input_files/Coords.XY"
 col_names=['node','X','Y']
 coo=pd.read_csv(fp_coo,names=col_names,skiprows=1,skipfooter=1,engine='python',skipinitialspace=True)
+coo[["X","Y"]]=coo[["X","Y"]].astype(str)
 
 
-# In[593]:
+# In[141]:
 
 coo.info()
 
 
-# In[594]:
+# In[142]:
 
 coo.tail()
 
 
 # ## Create first GIS data (straight links)
 
-# In[595]:
+# In[143]:
 
 straight0=data[["A_node","B_node"]]
-straight1=straight0.merge(coo,how="left",left_on="A_node",right_on="node")
-straight2=straight0.merge(coo,how="left",left_on="B_node",right_on="node")
+A=straight0.merge(coo,how="left",left_on="A_node",right_on="node").drop(["node"],axis=1)
+B=straight0.merge(coo,how="left",left_on="B_node",right_on="node").drop(["node"],axis=1)
+A['key']=A["A_node"]+"_"+A["B_node"]
+B['key']=B["A_node"]+"_"+B["B_node"]
 
 
-# In[596]:
+# In[162]:
 
-straight=pd.concat([straight1,straight2]).drop(['node'],axis=1).rename_axis({"X":1,"Y":2},axis=1)
+A.head(60)
 
 
-# In[597]:
+# In[145]:
 
-straight.info()
+B.info()
 
 
 # ## Read GIS data (curvy links)
 
-# In[598]:
+# In[146]:
 
 fp_gis="Input_files/Network_Shaping.gis"
 col_widths=[5,5,10,10,10,10,10,10,10]
 gis=pd.read_fwf(fp_gis,widths=col_widths,skiprows=14,skipfooter=2,header=None)
 
 
-# In[599]:
-
-gis.info()
-
-
-# In[600]:
+# In[147]:
 
 gis[11]=gis[0]+gis[1]
 gis['key']=gis[0].isnull().cumsum()
 gis=gis[[0,1,11,2,3,4,5,6,7,8,'key']]
 
 
-# In[601]:
+# In[148]:
 
 gis.head()
 
 
-# In[602]:
+# In[149]:
 
 gis_header= gis[gis[0].isnull()]
 gis_header=gis_header.drop([0,11,3,4,5,6,7,8],axis=1)
 gis_header.columns=["A_node","B_node","key"]
-gis=gis.drop([0,1],axis=1)
+gis=gis.drop([0,1],axis=1).rename_axis({11:1},axis=1)
 
 
-# In[603]:
+# In[150]:
 
 gis_header.head()
 
 
-# In[604]:
+# In[163]:
 
 gis.head()
 
 
-# In[605]:
+# In[152]:
 
-#gis2 = gis_header.merge(gis[gis[11].notnull()], on='key').set_index(["A_node","B_node"])
-#gis2.index = gis2.index.map('_'.join)
-#gis2=gis2.drop(['key'],axis=1).rename_axis([0]).reset_index()
-#gis2=gis2.drop(['key'],axis=1).reset_index()
-gis2 = gis_header.merge(gis[gis[11].notnull()], on='key')
-gis2=gis2.drop(['key'],axis=1)
+gis2 = gis_header.merge(gis[gis[1].notnull()], on='key')
+gis2=gis2.drop(['key'],axis=1)#.dropna(axis=0, how='any')
 
 
-# In[606]:
+# In[153]:
 
-gis2.head()
-
-
-# In[627]:
-
-gis2.info()
+gis3=gis2[["B_node","A_node",1,2,3,4,5,6,7,8]].rename_axis({"B_node":"A_node","A_node":"B_node"},axis=1)
+gis4=pd.concat([gis2,gis3])
 
 
-# In[607]:
+# In[154]:
 
-gis_1=gis2[["A_node","B_node",11,2]].rename_axis({11:1},axis=1)
-gis_2=gis2[["A_node","B_node",3,4]].rename_axis({3:1,4:2},axis=1)
-gis_3=gis2[["A_node","B_node",5,6]].rename_axis({5:1,6:2},axis=1)
-gis_4=gis2[["A_node","B_node",7,8]].rename_axis({7:1,8:2},axis=1)
-gis3=pd.concat([gis_1,gis_2,gis_3,gis_4])
-gis3.dropna(axis=0, how='any',inplace=True)
+gis4.columns
 
 
-# In[608]:
+# In[155]:
 
-gis3.info()
-
-
-# In[609]:
-
-gis4=gis3[["B_node","A_node",1,2]].rename_axis({"B_node":"A_node","A_node":"B_node"},axis=1)
-gis5=pd.concat([gis3,gis4])
+#gis4["1"]=gis4[1]+","+gis4[2]
+#gis4["2"]=gis4[3]+","+gis4[4]
+#gis4["3"]=gis4[5]+","+gis4[6]
+#gis4["4"]=gis4[7]+","+gis4[8]
+#gis4=gis4.drop([1,2,3,4,5,6,7,8],axis=1)
 
 
-# In[610]:
+# In[156]:
 
-gis5.info()
+gis_1=gis4[["A_node","B_node",1,2]].values.tolist()
+gis_2=gis4[["A_node","B_node",3,4]].values.tolist()
+gis_3=gis4[["A_node","B_node",5,6]].values.tolist()
+gis_4=gis4[["A_node","B_node",7,8]].values.tolist()
+gis_f=[None]*len(gis_1)*4
+gis_f[::4]=gis_1
+gis_f[1::4]=gis_2
+gis_f[2::4]=gis_3
+gis_f[3::4]=gis_4
+gis_f[:3]
+
+
+# In[157]:
+
+gis=pd.DataFrame.from_records(gis_f).dropna(axis=0, how='any').rename_axis({0:"A_node",1:"B_node",2:"X",3:"Y"},axis=1)
+gis['key']=gis["A_node"]+"_"+gis["B_node"]
+
+
+# In[164]:
+
+gis.head(60)
 
 
 # ## Merge data
 
-# In[611]:
+# In[180]:
 
-direction=data[["A_node","B_node"]]
-direction["key"]=direction["A_node"]+"_"+direction["B_node"]
-
-
-# In[612]:
-
-straight_curvy=pd.concat([straight,gis5])
-straight_curvy["key"]=straight_curvy["A_node"]+"_"+straight_curvy["B_node"]
-
-
-# In[613]:
-
-straight_curvy.info()
+gps=gis.groupby("key")
+li=[]
+for i,r in A.iterrows():
+    li.append(r.tolist())
+    try:
+        for ri in gps.groups[r["key"]]:
+            li.append(gis.loc[ri,:].tolist())
+    except KeyError:
+        pass
+    li.append(B[B.loc[:,"key"]==r["key"]].values.flatten().tolist()) 
 
 
-# In[614]:
+# In[188]:
 
-df=direction.merge(straight_curvy,how="left",on="key")
-
-
-# In[615]:
-
-df.info()
-
-
-# In[616]:
-
-df=df[['A_node_x','B_node_x',1,2]].rename_axis({'A_node_x':'A_node','B_node_x':'B_node',1:"X",2:"Y"},axis=1)
-
-
-# In[617]:
-
-df=df.sort_values(['A_node','B_node'], ascending=[True, True]).reset_index(drop=True)
-
-
-# In[618]:
-
-df.head()
-
-
-# In[619]:
-
-df['key']=df["A_node"]+"_"+df["B_node"]
-data['key']=data["A_node"]+"_"+data["B_node"]
-dfdata=df.merge(data,how='left',on='key').drop(['A_node_y','B_node_y'],axis=1).rename_axis({'A_node_x':'A_node','B_node_x':'B_node'},axis=1)
-
-
-# In[620]:
-
-dfdata.head()
+total=pd.DataFrame(li,columns=["A_node","B_node","X","Y","key"])
+total.tail(100)
 
 
 # ## Write MID/MIF files
 
-# In[662]:
-
-df_sub1=dfdata.iloc[:,5:]
-df_sub2=dfdata.iloc[:,:2]
-df_mid=df_sub2.merge(df_sub1,left_index=True,right_index=True)
-df_mid.head()
-
-
-# In[663]:
+# In[ ]:
 
 #mid:
-df_mid.drop_duplicates().to_csv("SATURN_network.MID",index=False,header=False)
+data.to_csv("SATURN_network.MID",index=False,header=False)
 
 
-# In[664]:
+# In[186]:
 
 #dfdata=dfdata.set_index(["A_node","B_node"]).sort_index(level=["A_node","B_node"])
 
 
-# In[665]:
+# In[187]:
 
 #mif:
 with open("SATURN_network.MIF","w") as mid:
     mid.write("Version  1200\nCharset \"WindowsLatin1\"\nDelimiter \",\"\nCoordSys Earth Projection 8, 79, \"m\", -2, 49, 0.9996012717, 400000, -100000 Bounds (-7845061.1011, -15524202.1641) (8645061.1011, 4470074.53373)\nColumns 4\n  A_Node Integer\n  B_Node Integer\n  data_1 Float\n  data_2 Float\nData\n\n")
-    gps=dfdata.groupby('key')
+    gps=total.groupby('key')
     for k,v in gps.groups.iteritems():
         mid.write("Pline "+str(len(v))+"\n")
-        for point in v:
-            mid.write(" "+(str(dfdata.loc[point,'X'])+" ")[:9]+"  "+str(dfdata.loc[point,'Y'])+"\n")
+        for index in v:
+            mid.write(" "+(total.loc[index,'X']+" ")[:9]+"  "+total.loc[index,'Y']+"\n")
         mid.write("    Pen (1,2,0)\n")
     mid.close
 
